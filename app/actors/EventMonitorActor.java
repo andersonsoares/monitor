9b6bc95bf5287f1c6a8cb07da2009715210b3b74
Anderson Soares <aersandersonsoares@gmail.com>
2013-05-02 20:58:04 -0300

    refactoring EventMonitorActor

--- app/actors/EventMonitorActor.java
+++ app/actors/EventMonitorActor.java
@@ -27,38 +27,50 @@ public class EventMonitorActor extends UntypedActor {
 	
 	Datastore ds = Singletons.datastore;
 	
+	@SuppressWarnings("unchecked")
 	@Override
 	public void onReceive(Object object) {
 		
+		
 		EventDAO dao = new EventDAO();
+		
+		//verificar se as keywords que estao no Cache sao iguais as que estao ativas no banco
+		
+		List<Event> activeEvents = dao.listBySituation(Situation.STARTED);
+		
+		HashMap<String, HashMap<String, TypeEnum>> keywordMap = (HashMap<String, HashMap<String, TypeEnum>>) Cache.get("keywordMap");
+		Set<String> eventKeys = keywordMap.keySet();
+		
+		System.out.println("Active events on DB: "+activeEvents.size());
+		System.out.println("Active events on CACHE: "+eventKeys.size());
+		if (activeEvents.size() != eventKeys.size()) {
+			System.out.println("Syncronizing cached keywords");
+			syncCacheWith(activeEvents);
+			System.out.println("Restarting twitter streamming");
+			restartStream();
+		}
+		
+
+		
 		List<Event> events = dao.listBySituation(Situation.STARTED, Situation.NEVER_STARTED);
+		
 		Date now = new Date(System.currentTimeMillis());
 		
 		for (Event event : events) {
 			
-			@SuppressWarnings("unchecked")
-			HashMap<String, HashMap<String, TypeEnum>> keywordMap = (HashMap<String, HashMap<String, TypeEnum>>) Cache.get("keywordMap");
 			
 			if (event.getSituation().equals(Situation.NEVER_STARTED)) {
 				// Event Never Started
-				if (now.after(event.getStartDate())) {
+				if (now.after(event.getStartDate()) && now.before(event.getFinishDate())) {
+					System.out.println("Iniciando "+event.getName());
 					dao.setSituation(event.getId(), Situation.STARTED);
-					
-					keywordMap.put(event.getNameLowerCase(), event.getKeywords());
-					Cache.set("keywordMap", keywordMap);
-					
-					restartStream();
 					
 				}
 			} else {
 				// Event Started
 				if(now.after(event.getFinishDate())) {
+					System.out.println("Finalizando "+event.getName());
 					dao.setSituation(event.getId(), Situation.FINISHED);
-					
-					keywordMap.remove(event.getNameLowerCase());
-					Cache.set("keywordMap", keywordMap);
-					
-					restartStream();
 					
 				}
 			}
@@ -67,18 +79,35 @@ public class EventMonitorActor extends UntypedActor {
 		
 	}
 
-	private void restartStream() {
-		System.out.println("Restarting stream");
+	/**
+	 * Method that syncronize the cache with keywords of active events on DB
+	 * 
+	 * @param activeEvents
+	 */
+	private void syncCacheWith(List<Event> activeEvents) {
 		
+		HashMap<String, HashMap<String, TypeEnum>> keywordMap = new HashMap<String, HashMap<String, TypeEnum>>();
+		
+		for (Event event : activeEvents) {
+			
+			keywordMap.put(event.getName(), event.getKeywords());
+			
+		}
+		Cache.set("keywordMap", keywordMap);
+	}
+
+	private void restartStream() {
+
+		// Get Active Events on CACHE
 		@SuppressWarnings("unchecked")
 		HashMap<String, HashMap<String, TypeEnum>> keywordMap = (HashMap<String, HashMap<String, TypeEnum>>) Cache.get("keywordMap");
-		
+
+		// Separete Keywords(#hashtags / normal_keywords) from @users
 		Set<String> keys = keywordMap.keySet();
 		List<String> keywords = new ArrayList<String>();
 		List<String> users = new ArrayList<String>();
 		for (String key : keys) {
-//			keywords.addAll(keywordMap.get(key).keySet());
-			
+
 			HashMap<String, TypeEnum> map = keywordMap.get(key);
 			Set<String> ks = map.keySet();
 			for (String k : ks) {
@@ -93,17 +122,23 @@ public class EventMonitorActor extends UntypedActor {
 			}
 		}
 		
-		System.out.println("Active keywords: "+keywords.size());
-		for (String string : keywords) {
-			System.out.println(string);
-		}
-		System.out.println("Active users: "+users.size());
-		for (String string : users) {
-			System.out.println(string);
-		}
 		
+		restartTwitterStream(keywords);
+		
+		restartUserStream(users);
 		
 		
 	}
+
+	private void restartTwitterStream(List<String> keywords) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void restartUserStream(List<String> users) {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
