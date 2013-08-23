f150e0af3834eb5c4725f4c8199bb6ca1935ce75
Anderson Soares <aersandersonsoares@gmail.com>
2013-08-23 10:27:21 -0300

    criado lista de stemmes negativos/positivos e enum para classificar sentimento

---
+++ app/enums/SentimentEnum.java
@@ -0,0 +1,13 @@
+package enums;
+
+
+/**
+ * Enum class that represent a Sentiment, that can be positive, negative or neutral
+ * @author aers
+ *
+ */
+public enum SentimentEnum {
+	
+	POSITIVE, NEGATIVE, NEUTRAL
+
+}
